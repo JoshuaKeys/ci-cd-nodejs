b9e5257fbc288f886f674c628330d6c75dcbada0
Joshua Oguma <avwerosuoghene.oguma1@gmail.com>
2023-05-04 00:49:57 +0300

    added some changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const server = app.listen(3000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Example app listening at http://%s:%s`, host, port);
+});
